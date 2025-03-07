0a72ca43a7cc0e81274ca995831b8e80f941b082
Nattapat <nattapat@4plus.co.th>
2025-03-07 13:08:30 +0700

    Create py file

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ resource "aws_s3_bucket" "test-tf-bucket" {
   }
 }
 
-# resource "aws_s3_object" "example" {
-#   key                    = "someobject"
-#   bucket                 = aws_s3_bucket.examplebucket.id
-#   source                 = "index.html"
+resource "aws_s3_object" "api_ingest" {
+  key                    = "scripts/api_ingest.py"
+  bucket                 = "aws-glue-assets-863518427397-ap-southeast-1"
+  source                 = ".github/workflows/deploy.yaml"
 #   server_side_encryption = "aws:kms"
-# }
+}
